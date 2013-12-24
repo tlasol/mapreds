eccf648f33c3bab9bdaf2f7aeeef13fb390e29c0
Mary Galiulina <tesseractum@gmail.com>
2013-12-24 17:21:25 +0300

    IT WORKS!!!!!!!!!

--- src/main/java/com/mary/mapr/WordCount.java
+++ src/main/java/com/mary/mapr/WordCount.java
@@ -1,11 +1,11 @@
+package com.mary.mapr;
+
 import java.io.IOException;
 import java.util.*;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
 
 public class WordCount {
 
